32363abdbfe9364b95c4db66c93c1826eeed181e
Ahmaad Ansari <ahmaad.ali.ansari@hotmail.com>
2024-03-19 14:28:47 -0400

    Commit

---
+++ launch/r2ware_status_launch.py
@@ -0,0 +1,22 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Include Ackman_driver_R2 node
+        Node(
+            package='r2ware_status',
+            executable='r2ware_status_node',
+            output='screen',
+            prefix=[],
+        ),
+    ])
